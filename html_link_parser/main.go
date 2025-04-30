a9d81632df23924286900099c217b0e1cf44de93
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2025-04-30 13:44:47 -0300

    html_link_parser

--- html_link_parser/main.go
+++ html_link_parser/main.go
@@ -3,8 +3,32 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/html"
+	"os"
+	"strings"
 )
 
+type Link struct {
+	href string
+	text string
+}
+
 func main() {
-	fmt.Print("oi")
+	input_file := "ex1.html"
+	content, _ := os.ReadFile(input_file)
+	reader := strings.NewReader(string(content))
+	doc, err := html.Parse(reader)
+	if err != nil {
+		panic("Error parsing html file")
+	}
+	for n := range doc.Descendants() {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			new_link := Link{}
+			for _, attr := range n.Attr {
+				if attr.Key == "href" {
+					new_link.href = attr.Val
+				}
+			}
+			fmt.Println(n.Descendants())
+		}
+	}
 }
